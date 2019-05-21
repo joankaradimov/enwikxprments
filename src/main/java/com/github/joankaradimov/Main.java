6275e657a2ed1a353b98b6ed08cf1ba9569b5982
Joan Karadimov <joan.karadimov@gmail.com>
2019-05-21 21:46:30 +0300

    Create a valid XML stream containing enwik8

--- src/main/java/com/github/joankaradimov/Main.java
+++ src/main/java/com/github/joankaradimov/Main.java
@@ -1,6 +1,14 @@
 package com.github.joankaradimov;
 
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
+        try (FileInputStream stream = new FileInputStream("build/enwik8")) {
+            ByteArrayInputStream xmlClosingElements = new ByteArrayInputStream("</text></revision></page></mediawiki>".getBytes());
+            SequenceInputStream validXmlStream = new SequenceInputStream(stream, xmlClosingElements);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
