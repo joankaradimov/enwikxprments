aaa5677e8a024cd118dbdeba08df0b98586a44e4
Joan Karadimov <joan.karadimov@gmail.com>
2020-04-20 14:56:32 +0300

    Implement ISO date time parsing in C++

--- enwikxprments/compress/compress.cpp
+++ enwikxprments/compress/compress.cpp
@@ -1,7 +1,40 @@
+#include <ctime>
 #include <fstream>
 #include <iostream>
 
 #include "xml/parser"
+
+struct IsoDateTime {
+    time_t time;
+
+    IsoDateTime(time_t time): time(time) {}
+    IsoDateTime(tm time) : IsoDateTime(mktime(&time)) {}
+};
+
+namespace xml
+{
+    template <> struct value_traits<IsoDateTime>
+    {
+        static IsoDateTime parse(std::string s, const parser& p)
+        {
+            tm time;
+
+            sscanf_s(s.c_str(), "%4d-%2d-%2dT%2d:%2d:%2dZ",
+                &time.tm_year, &time.tm_mon, &time.tm_mday,
+                &time.tm_hour, &time.tm_min, &time.tm_sec);
+
+            time.tm_year -= 1900;
+            time.tm_mon -= 1;
+
+            return time;
+        }
+
+        static std::string serialize(IsoDateTime x, const serializer&)
+        {
+            return ""; // TODO
+        }
+    };
+}
 
 int main()
 {
@@ -59,7 +92,7 @@ int main()
 
                     enwik_parser.next_expect(xml::parser::start_element, ns, "timestamp", xml::content::simple);
                     enwik_parser.next_expect(xml::parser::characters);
-                    auto timestamp = enwik_parser.value(); // TODO: parse time
+                    auto timestamp = enwik_parser.value<IsoDateTime>();
                     enwik_parser.next_expect(xml::parser::end_element);
 
                     enwik_parser.next_expect(xml::parser::start_element, ns, "contributor", xml::content::complex);
