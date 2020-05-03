6feeb4f526d1a9b93c9682cd744e455dd2afed18
Joan Karadimov <joan.karadimov@gmail.com>
2020-05-03 03:58:26 +0300

    Use a function try block in main

--- enwikxprments/compress/compress.cpp
+++ enwikxprments/compress/compress.cpp
@@ -3,18 +3,16 @@
 #include "page_revision.hpp"
 
 int main()
-{
+try {
     const char* input_path = "C:\\Users\\joank\\work\\enwikxprments\\build\\enwik9";
 
     PageRevisions page_revisions;
 
-    try {
-        page_revisions.parse_xml(input_path);
-        page_revisions.dump();
-    }
-    catch (xml::parsing& error) {
-        std::cout
-            << "Parsing error at position (" << error.line() << "," << error.column()
-            << "); message: " << error.description() << std::endl;
-    }
+    page_revisions.parse_xml(input_path);
+    page_revisions.dump();
+}
+catch (xml::parsing & error) {
+    std::cout
+        << "Parsing error at position (" << error.line() << "," << error.column()
+        << "); message: " << error.description() << std::endl;
 }
